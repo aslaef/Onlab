ef132eacd4d565eff9bf8fa5a42dc1b03f643cda
aslaef <trefusz@gmail.com>
2018-04-09 18:25:09 +0200

    details, heatmap, parking
    some new stuff added

--- Parser.py
+++ Parser.py
@@ -1,38 +1,24 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from datetime import datetime
 import json
 
 app = Flask(__name__)
+app.FLASK_DEBUG=1
 
 def date_assembler(string):
-    if(len(string)>=5):
-        substr = str[-19::]
-        dt = datetime.strptime(substr, '%Y_%m_%d_%H_%M')
-        return dt
-    else:return datetime.strptime('2018_02_20_10_30', '%Y_%m_%d_%H_%M')
+    try:
+        if(len(string)>=5):
+            dt = datetime.strptime(string, '%Y.%m.%d.%H.%M.%S')
+            return dt
+        else:
+            return datetime.strptime('2018_02_20_10_30_00', '%Y.%m.%d.%H.%M.%S')
+    except Exception as e:
+        print(e)
 
 
-
-
-
-@app.route('/<value>')
-def heatmap(value):
-    if ("noise" in str(value)):
-        #date = date_assembler(str(value))
-        return render_template('noise.html')
-    if ("heat" in str(value)):
-        #date = date_assembler(str(value))
-        return render_template('heat.html')
-    if ("smoke" in str(value)):
-        #date = date_assembler(str(value))
-        return render_template('smoke.html')
-    if ("light" in str(value)):
-        #date = date_assembler(str(value))
-        return render_template('light.html')
-    return 404
-
-
-
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/heat', methods=['POST','GET'])
@@ -40,10 +26,21 @@ def heatmap_post():
     try:
         if(request.method == 'POST'):
             text = request.form.get("datefield")
-            print(str(text))
+
+            mydate_string=str(text)
+
+            #TODO:
+
+
+
+            requested_date = date_assembler(mydate_string)
+
             data=[[19.9240094, 47.780369, 15.9]]
 
             #heatmap database
+
+
+
 
 
             return render_template('heat.html', data=json.dumps(data))
@@ -52,6 +49,57 @@ def heatmap_post():
     except Exception as e:
         print(e)
         return render_template('heat.html')
+
+@app.route('/noise', methods=['POST','GET'])
+def noise_post():
+    try:
+        if(request.method == 'POST'):
+            text = request.form.get("datefield")
+            print(str(text))
+            data=[[19.9240094, 47.780369, 15.9]]
+
+            #heatmap database
+
+            return render_template('noise.html', data=json.dumps(data))
+        else:
+            return render_template('noise.html')
+    except Exception as e:
+        print(e)
+        return render_template('noise.html')
+
+@app.route('/smoke', methods=['POST','GET'])
+def smoke_post():
+    try:
+        if(request.method == 'POST'):
+            text = request.form.get("datefield")
+            print(str(text))
+            data=[[19.9240094, 47.780369, 15.9]]
+
+            #heatmap database
+
+            return render_template('smoke.html', data=json.dumps(data))
+        else:
+            return render_template('smoke.html')
+    except Exception as e:
+        print(e)
+        return render_template('smoke.html')
+
+@app.route('/light', methods=['POST','GET'])
+def light_post():
+    try:
+        if(request.method == 'POST'):
+            text = request.form.get("datefield")
+            print(str(text))
+            data=[[19.9240094, 47.780369, 15.9]]
+
+            #heatmap database
+
+            return render_template('light.html', data=json.dumps(data))
+        else:
+            return render_template('light.html')
+    except Exception as e:
+        print(e)
+        return render_template('light.html')
 
 if __name__ == "__main__":
     app.run()
